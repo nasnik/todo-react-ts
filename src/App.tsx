1ac1ee3ed84b91833525e2a3e1d651040f86c067
nasnik <nikoulkina@gmail.com>
2024-11-01 14:52:12 -0700

    todoList rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+interface TodoItem{
+    id: number;
+    title: string;
+}
 function App() {
-  const [count, setCount] = useState(0)
-
+    const todoList: TodoItem[] = [
+        {id: 1, title: 'Learn React'},
+        {id: 2, title: 'Learn NodeJS'},
+        {id: 3, title: 'Learn JavaScript'}
+    ]
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1>Todo List</h1>
+        <ul>
+            {todoList.map(item =>
+                <li key={item.id}>{item.title}</li>
+            )}
+        </ul>
     </>
   )
 }
